88944e1729a9929662a077dab28b0afab62b318e
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-06-22 21:24:27 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,7 +6,7 @@ class Triangle
   
   def initialize(length_one, length_two, length_three)
     @lengths = [length_one, length_two, length_three]
-    @lenghts.sort
+    @lengths.sort!
   end
   
   
