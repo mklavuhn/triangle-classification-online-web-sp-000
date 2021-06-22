779dc94723638e7b3ea578026ab1dd2c8bc858aa
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-06-22 21:26:14 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :lengths
+  
+  @lengths = []
+  
+  def initialize(length_one, length_two, length_three)
+    @lengths = [length_one, length_two, length_three]
+    @lengths.sort!
+  end
+  
+  
+  def kind 
+    if @lengths.any?{|side| side <= 0} || ((@lengths[0] + @lengths[1]) <= @lengths[2])
+      raise TriangleError
+    elsif @lengths.uniq.length == 1
+      :equilateral
+    elsif @lengths.uniq.length == 2
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  
+  class TriangleError < StandardError
+    
+  end
+  
+  
 end
