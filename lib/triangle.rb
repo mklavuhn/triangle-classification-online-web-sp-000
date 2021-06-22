faa6d0a09c8daa840d6635861fbfdf9a7e246d5f
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-06-22 21:09:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,8 +1,13 @@
 class Triangle
   
   def initialize(length_one, length_two, length_three)
-    
+    @length_one = length_one
+    @length_two = length_two
+    @length_three = length_three
   end
+  
+  def kind 
+    
     
   
   
