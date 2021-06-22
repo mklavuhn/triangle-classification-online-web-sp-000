6d33a6f8ee173b3a1f5a0871963994b5e6a8d300
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-06-22 21:10:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,13 +1,14 @@
 class Triangle
   
-  def initialize(length_one, length_two, length_three)
+  def initialize(length_one:, length_two:, length_three:)
     @length_one = length_one
     @length_two = length_two
     @length_three = length_three
   end
   
   def kind 
-    
+    if length_three == length_two == length_one
+      equilateral
     
   
   
